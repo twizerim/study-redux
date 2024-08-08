686ea64d369ae4f9c8564bcadfce285c8ebc75b5
TWIZERIMANA Gedeon <136312774+twizerim@users.noreply.github.com>
2024-08-08 17:22:35 +0200

    Create main.yml
    addition of set up workflow

--- src/pages/houses/[id].tsx
+++ src/pages/houses/[id].tsx
@@ -1,5 +1,6 @@
 import { useGetHouseDetailsQuery } from "@/game/api/apiSlice";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const HouseDetails = () => {
   const router = useRouter();
@@ -10,11 +11,12 @@ const HouseDetails = () => {
     isLoading,
   } = useGetHouseDetailsQuery(id as string);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p className="text-center text-red-400">Loading...</p>;
   if (error) return <p>An error occurred</p>;
 
   return (
-    <div className="container mx-auto p-4">
+     <div className="flex justify-center items-center h-[80vh] ">
+        <div className="container mx-auto p-4  w-[50%] rounded-[10px] shadow-md">
       <h1 className="text-2xl font-bold mb-4">{house.name}</h1>
       <p>
         <strong>Region:</strong> {house.region}
@@ -32,6 +34,7 @@ const HouseDetails = () => {
         <strong>Founded:</strong> {house.founded}
       </p>
     </div>
+     </div>
   );
 };
 
